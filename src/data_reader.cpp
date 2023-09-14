9f09fd8b097cbddeb1936f2c4227f9e0f7c66053
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2023-09-14 21:53:54 +0300

    Add exception handling and now returns number of nodes in graph

--- src/data_reader.cpp
+++ src/data_reader.cpp
@@ -3,15 +3,35 @@
 
 #include <fstream>
 #include <string>
+#include <unordered_set>
 
-void DataReader::readEntries(std::string& inputFileName, Ledger& ledger) {
+int DataReader::readEntries(const std::string& inputFileName, std::vector<std::tuple<std::string, std::string, int>>& entries) {
     std::ifstream inputFile(inputFileName);
 
-    std::string debtor, creditor;
-    int money;
-    while (inputFile >> debtor >> creditor >> money) {
-        ledger.addEntry(debtor, creditor, money);
+    int partakers = 0;
+    std::unordered_set<std::string> set;
+    std::string debtor, creditor, moneyStr;
+    while (inputFile >> debtor >> creditor >> moneyStr) {
+        for (char c : moneyStr) {
+            if (!isdigit(c)) {
+                throw std::invalid_argument("Invalid moneyFormat exception");
+            }
+        }
+        if (moneyStr.size() > 7) {
+            throw std::invalid_argument("Invalid moneyAmount exception");
+        }
+        int money = std::stoi(moneyStr);
+        entries.emplace_back(debtor, creditor, money);
+        if (set.find(debtor) == end(set)) {
+            partakers++;
+            set.insert(debtor);
+        }
+        if (set.find(creditor) == end(set)) {
+            partakers++;
+            set.insert(creditor);
+        }
     }
 
     inputFile.close();
+    return partakers;
 }
