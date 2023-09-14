feac91f59c54cbbfdf152cd4cfcf9ba8c28f3641
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2023-09-14 21:58:12 +0300

    Add endline after printing the ledger

--- src/data_writer.cpp
+++ src/data_writer.cpp
@@ -1,13 +1,14 @@
 #include "../include/data_writer.h"
+#include "../include/ledger.h"
 
 #include <iostream>
 #include <fstream>
 
 void DataWriter::writeLedger(std::ostream& os, Ledger& ledger) {
-    os << ledger;
+    os << ledger << std::endl;
 }
 
-void DataWriter::writeLedger(std::string outputFileName, Ledger& ledger) {
+void DataWriter::writeLedger(const std::string& outputFileName, Ledger& ledger) {
     std::ofstream outputFile(outputFileName);
     writeLedger(outputFile, ledger);
     outputFile.close();
